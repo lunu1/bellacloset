e0d1c04ce9b62ea62572b61bc03fdfb781a1b8da
ARchanaarchu2001 <archanaarchu1797@gmail.com>
2025-09-26 17:36:13 +0400

    changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -58,8 +58,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"], // <-- important
+  
 }));
 
 
